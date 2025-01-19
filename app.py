a9516e9feafcd819e70aeec7e8fef59972f620a5
RkcBusterz <manasichand@outlook.com>
2025-01-19 14:13:48 +0530

    fixed app.py and adding admin cog

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ load_dotenv()
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="ptero", intents=intents)
 
-def load_cogs():
+async def load_cogs():
     try:
-        bot.load_extension("cogs.admin")
+        await bot.load_extension("cogs.admin")
         print("Loaded cog: admin")
     except Exception as e:
         print(f"Failed to load cog 'admin': {e}")
 
 @bot.event
 async def on_ready():
-    load_cogs()
+    await load_cogs()
     print(f"Logged in as {bot.user}")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
